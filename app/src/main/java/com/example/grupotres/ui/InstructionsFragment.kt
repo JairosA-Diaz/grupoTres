1db35281f85d2f93435d2483fc022014d20bcadd
Jairo <jairo.diaz.diaz@correounivalle.edu.co>
2026-05-28 00:44:01 -0500

    HU 5-0 Criterio #3: Correccíon toolbar personalizada con el título Reglas del Juego

--- app/src/main/java/com/example/grupotres/ui/InstructionsFragment.kt
+++ app/src/main/java/com/example/grupotres/ui/InstructionsFragment.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.grupotres.R
@@ -21,7 +21,8 @@ class InstructionsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.btn_back_home).setOnClickListener {
+        // Configura el botón de la flecha atrás en el toolbar
+        view.findViewById<ImageView>(R.id.iv_back).setOnClickListener {
             // Regresar al Home usando el NavController
             findNavController().navigateUp()
         }
